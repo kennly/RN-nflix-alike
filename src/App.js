92165338f389cf20c5ee1521d3ee596c75d1f353
Kenneth A <kennethau82@gmail.com>
2017-05-09 21:42:01 +1000

    Slider: Added a swiper with dummy Image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import List from './components/List';
+import Slider from './components/Slider'
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Slider />
         <List />
       </View>
     );
